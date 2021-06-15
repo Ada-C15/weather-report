3fa177a9bb9a9d0a6253513897118061e4930ed8
Glenda <chicag9313@gmail.com>
2021-06-15 15:26:13 -0700

    Made the increase arrow work for the temp and to display.

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,17 @@
-
+var tempTrack = 0
+const incrementTemp = () => {
+    const newTemp = document.createElement("span");
+    const tempContainor = document.querySelector("#add-temp");
+    const tempCountContainer = document.querySelector("#tempCount")
+    tempTrack += 1;
+    tempCountContainer.textContent = `${tempTrack}`;
+    console.log("It Works")
+    
+    console.log(tempTrack)
+};
 const registerEventHandlers = () => {
-    const crabButton = document.querySelector("#up-arrow");
-    crabButton.addEventListener("click", upArrow);
+    const upArrow = document.querySelector("#add-temp");
+    upArrow.addEventListener("click", incrementTemp);
 };
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
+
