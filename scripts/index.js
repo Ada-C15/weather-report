6d4006c1aa770ab1a2d5799ee3d9e4b01d2b3852
halisa <halisasaipulaa@gmail.com>
2021-06-15 16:25:33 -0700

    update color of temp num

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,7 @@ let temp = 71;
 const update = temp=>{
     const tempContainer=document.getElementById("temp")
     tempContainer.textContent=`${temp}`;
+    updateColorofTemp(temp);
 }
 
 const increatTemp = ()=>{
@@ -12,6 +13,23 @@ const decreatTemp = ()=>{
     temp -=1;
     update(temp)
 };
+const updateColorofTemp=(currentTemp)=>{
+    const tempContainer=document.getElementById("temp");
+    let color="teal";
+    if (currentTemp>=100) {
+        color="red";
+    }else if(currentTemp>=90){
+        color="purple";
+    }else if(currentTemp>=80){
+        color="blue"
+    }else if(currentTemp>=70){
+        color="green"
+    }else if(currentTemp>=60){
+        color="orange"
+    }
+    tempContainer.classList=color;
+}
+
 const registerEventHandlers=()=>{
     update(temp);
     const increaseTempValue=document.getElementById("plustemp")
