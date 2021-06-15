34fa238fab2c27d0b6d89aab2790f824c0635c0e
Sidney DuPont <sidneyadupont@gmail.com>
2021-06-15 14:16:03 -0700

    Adds functionality for landscape changing based on current temperature

--- scripts/index.js
+++ scripts/index.js
@@ -4,29 +4,64 @@ let currentTemp = 67;
 const updateTemperature = currentTemp => {
     const tempValue = document.getElementById("temp-integer");
     tempValue.textContent = currentTemp;
+    changeTempColor(currentTemp);
+    changeLandscape(currentTemp);
 }
 
-// increases temperature
+// increases temperature by 1
 const increaseTemperature = () => {
     currentTemp += 1;
     updateTemperature(currentTemp);
 };
 
+// decreases the temp by 1
 const decreaseTemperature = () => {
     currentTemp -= 1;
     updateTemperature(currentTemp);
 };
 
-// const changeTempColor = (currentTemp)
+// changes the font color of the temperature based on how hot or cold it is
+const changeTempColor = currentTemp => {
+    // const tempValue = document.getElementById("temp-integer");
+    let color = "";
+    if (currentTemp > 80) {
+        color = "red"
+    } else if (currentTemp >= 70) {
+        color = "orange"
+    } else if (currentTemp >= 60) {
+        color = "yellow"
+    } else if (currentTemp >= 50) {
+        color = "green"
+    } else if (currentTemp < 50) {
+        color = "teal"
+    }
+    currentTemp.classList = color;
+}
+
+const changeLandscape = currentTemp => {
+    const landscapeContainer = document.getElementById("landscape");
+    let landscape = "";
+    if (currentTemp >= 80) {
+        landscape = "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂"
+    } else if (currentTemp >= 70) {
+        landscape = "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷"
+    } else if (currentTemp >= 60) {
+        landscape = "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃"
+    } else if (currentTemp < 60) {
+        landscape = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲"
+    }
+    landscapeContainer.textContent = landscape;
+}
 
 const changeSky = () => {
     const skyDropdown = document.getElementById("sky-dropdown").value;
     const skyForecast = document.getElementById("sky");
     // why do we make this variable rather than just use skyForecast and manipulate that directly?
-    let sky = "☁️ ☁️ ☁️ ☀ ☁️ ☁️";
+    let sky = "";
     let skyColor = "";
     if (skyDropdown === "Sunny") {
-        sky = "☁️ ☁️ ☁️ ☀ ☁️ ☁️"
+        sky = "☁️ ☁️ ☁️ ☀️ ☁️ ☁️"
+        // why do these need to be added as classes in CSS?
         // skyColor = "sunny";
     }
     else if (skyDropdown === "Cloudy") {
