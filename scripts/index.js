b6e458483798467b605d25949711e7fdcb4189d9
alison <alimcginley@gmail.com>
2021-06-15 18:21:40 -0700

    updates submit button and switches to reset after a new city is submitted

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
 const state = {
     temperature: 45,
+    city: "Seattle",
+    cityButton: "Submit"
 }
 
 const updateTemperature = () => {
@@ -7,18 +9,32 @@ const updateTemperature = () => {
     const temperatureElement = document.querySelector("#currentTemp")
     temperatureElement.textContent = `${state.temperature}`
 }
+
 const downTemperature = () => {
     state.temperature -= 1;
     const temperatureElement = document.querySelector("#currentTemp")
     temperatureElement.textContent = `${state.temperature}`
-
 }
+
+const updateCity = () => {
+    const userCity = document.querySelector("#userCity").value;
+    state.city=`${userCity}`;
+    const cityElement = document.querySelector("#defaultCity");
+    cityElement.textContent = `How is the weather today in ${state.city}?`;
+    state.cityButton = "Reset"
+    const cityButtonElement = document.querySelector("#submitCity")
+    cityButtonElement.textContent = `${state.cityButton}`
+}
+
+
 
 const registerEventHandlers = () => {
     const upTemperatureButton = document.querySelector("#upTemperature");
     upTemperatureButton.addEventListener("click", updateTemperature)
     const downTemperatureButton = document.querySelector("#downTemperature");
-    downTemperatureButton.addEventListener('click', downTemperature)
+    downTemperatureButton.addEventListener('click', downTemperature);
+    const submitCityButton = document.querySelector("#submitCity");
+    submitCityButton.addEventListener('click', updateCity)
 
 }
 
