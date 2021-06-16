9ab38b354b36f4a15023420e032148a79c7259dd
Hena <lee.henajane@gmail.com>
2021-06-15 21:51:45 -0500

    Adds image files

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 const state = {
     temperature: 70,
-    newLocation: ''
+    location: "New York",
+    sky: "rainy"
 };
 
 const increaseTemperature = () => {
@@ -25,13 +26,27 @@ const updateLocation = () => {
 
 }
 
+const updateSky = () => {
+    const selection = document.querySelector("value")
+    const sky = document.querySelector("#display")
+    if (selection.value == "rainy"){
+        sky.style.background = url('rain.gif');
+    } else if (selection.value == "sunny") {
+        sky.style.background = url('sunny.gif');
+    } else if (selection.value == "overcast"){
+        sky.style.background == url("nightsky.gif"); {}
+    } else if (selection.value == "dawn") {
+        sky.style.background = url('dawn.gif')
+    }
+}
+
 const registerEventHandlers = () => {
     const upButton = document.querySelector("#upButton");
     upButton.addEventListener("click", increaseTemperature);
     const downButton = document.querySelector("#downButton");
     downButton.addEventListener("click", decreaseTemperature);
-    const updateLocationButton = document.querySelector("#submit");
-    updateLocationButton.addEventListener("click", updateLocation)
+    // const updateLocationButton = document.querySelector("#submit");
+    // updateLocationButton.addEventListener("click", updateLocation)
 };
 
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
