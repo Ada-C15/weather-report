1fb72423ed7677cfb52ac1ab839455b2d1aa3050
maite <arp.maite@gmail.com>
2021-06-15 17:57:54 -0700

    changes landscape

--- scripts/index.js
+++ scripts/index.js
@@ -4,12 +4,20 @@ const state = {
 
 
 const temperature = document.querySelector("#temp_number");
+const landscape = document.querySelector("#ground");
+const airspace = document.querySelector("#sky");
 
 const temperatureUp = () => {
     state.temp_number += 1;
     temperature.textContent = `${state.temp_number}`;
 };
 //tempUpButton.addEventListener("click", temperatureUp);
+const temperatureDown = () => {
+    state.temp_number -= 1;
+    temperature.textContent = `${state.temp_number}`;
+};
+//tempDownButton.addEventListener("click", temperatureDown);
+
 const tempChangeColor = () => {
     if (state.temp_number > 79) {
         temperature.className = "red";
@@ -32,21 +40,54 @@ const tempChangeColor = () => {
 
 // const tempDownButton = document.querySelector("#down");
 // tempDownButton.addEventListener("click", tempChangeColor);
-const temperatureDown = () => {
-    state.temp_number -= 1;
-    temperature.textContent = `${state.temp_number}`;
+const tempChangeGround = () => {
+    if (state.temp_number > 79) {
+        landscape.textContent = "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂"; 
+    }
+    else if (state.temp_number > 69) {
+        landscape.textContent = "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷"; 
+    }
+    else if (state.temp_number > 59) {
+        landscape.textContent = "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃"; 
+    }
+    else if (state.temp_number > 49) {
+        landscape.textContent = "🌲🍂🍂🌲🍁🍂🍂🍂🍁🌲🍂🍂🌲"; 
+    }
+    else if (state.temp_number > 48) {
+        landscape.textContent = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲"; 
+    }
 };
-//tempDownButton.addEventListener("click", temperatureDown);
+
+// document.querySelector("sky_options").onchange = () => {
+//     const sky = document.querySelector("sky_options").value;
+//     sky.value = sky.value.tempChangeSky()
+// };
+
+// const tempChangeSky = () => {
+//     if (sky.value === "sunny") {
+//         airspace.textContent = "☁️ ☁️ ☁️ ☀️ ☁️ ☁️"; 
+//     }
+//     else if (state.temp_number > 59) {
+//         airspace.textContent = "☁️☁️ ☁️ ☁️☁️ ☁️ 🌤 ☁️ ☁️☁️"; 
+//     }
+//     else if (state.temp_number > 49) {
+//         airspace.textContent = "🌧🌈⛈🌧🌧💧⛈🌧🌦🌧💧🌧🌧"; 
+//     }
+//     else if (state.temp_number > 48) {
+//         airspace.textContent = "🌨❄️🌨🌨❄️❄️🌨❄️🌨❄️❄️🌨🌨"; 
+//     }
+// };
 
 const registerEventHandlers = () => {
     const tempUpButton = document.querySelector("#up");
     tempUpButton.addEventListener("click", temperatureUp);
     tempUpButton.addEventListener("click", tempChangeColor);
+    tempUpButton.addEventListener("click", tempChangeGround);
 
     const tempDownButton = document.querySelector("#down");
     tempDownButton.addEventListener("click", temperatureDown);
     tempDownButton.addEventListener("click", tempChangeColor);
-
+    tempDownButton.addEventListener("click", tempChangeGround);
 };
 
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
