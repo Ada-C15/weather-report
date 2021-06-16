abb9750ce7324e76951bac55f7af822e3574f155
x <57688724+AeronRoemer@users.noreply.github.com>
2021-06-15 19:59:56 -0600

    All Waves but 3

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,53 @@
 const skyBackground = document.querySelector('.sky-display');
 const cityName = document.querySelector('.city-name');
 const cityNameForm = document.querySelector('#city-name-form');
-const temp = document.querySelector('.temp');
+const temp = document.querySelector('#temp');
+const plus = document.querySelector('#plus');
+const minus = document.querySelector('#minus');
 const landscape = document.querySelector('.landscape');
 const skyMenu = document.querySelector('.sky-menu');
 
 cityNameForm.addEventListener('submit', (event) => {
     let info = new FormData(cityNameForm)
+    for (var value of info.values()) {
+        cityName.textContent = value;
+    } 
+    cityNameForm.style.display = 'none';  
     event.preventDefault();
+});
+
+cityName.addEventListener('click', (event) => {
+// add cursor style pointer to h1 for ease of use
+    cityNameForm.style.display = 'block';  
 
 });
 
-cityNameForm.addEventListener('formdata', (e) => {
-    console.log('formdata fired');
-  
-    // Get the form data from the event object
-    let data = e.formData;
-    for (var value of data.values()) {
-        cityName.textContent = value;
-    } 
-    cityNameForm.style.display = 'none';   
-  
-  });
+const emojiDisp = function (temp){
+    switch(true){
+        case (temp <= 59):
+            landscape.textContent = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
+            break;
+        case (temp <= 69):
+            landscape.textContent = "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃";
+            break;
+        case (temp <= 79):
+            landscape.textContent = "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷";
+            break;
+        case (temp > 79):
+            landscape.textContent = "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂";
+    }
+}
+
+plus.addEventListener('click', (event) => {
+    // add cursor style pointer to span for ease of use
+    let temperature = parseInt(temp.textContent)
+    temp.textContent = temperature + 1
+    emojiDisp(temperature + 1)
+    });
+
+minus.addEventListener('click', (event) => {
+    // add cursor style pointer to span for ease of use
+    let temperature = parseInt(temp.textContent)
+    temp.textContent = temperature - 1
+    emojiDisp(temperature - 1)
+    });
