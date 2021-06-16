dc053d2101733d479a3ebecbce788366880b38ab
alison <alimcginley@gmail.com>
2021-06-15 22:00:16 -0700

    gets temperature background started

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,29 @@
 const state = {
     temperature: 45,
     city: "Seattle",
-    cityButton: "Submit"
+    cityButton: "Submit",
+    tempClasses: "box coldWeather"
 }
 
 const updateTemperature = () => {
     state.temperature += 1
+    changeTempBackground()
     const temperatureElement = document.querySelector("#currentTemp")
-    temperatureElement.textContent = `${state.temperature}`
+    temperatureElement.textContent = `${state.temperature}`;
+}
+
+const changeTempBackground = () => {
+    const temperatureBox = document.querySelector("#temperature")
+    if (state.temperature >=80) {
+        state.tempClasses = `box tooHot`;
+    } else if (79 >= state.temperature >= 70) {
+        state.tempClasses = `box notTooHot`;
+    } else if (69 >= state.temperature >= 60){
+        state.tempClasses = `box quiteCool`;
+    } else if (59 >= state.temperature >= 50){
+        state.tempClasses = `box jacketWeather`;
+    }
+    return state.tempClasses
 }
 
 const downTemperature = () => {
@@ -17,25 +33,28 @@ const downTemperature = () => {
 }
 
 const updateCity = () => {
-    const userCity = document.querySelector("#userCity").value;
-    state.city=`${userCity}`;
+    const cityButtonElement = document.querySelector("#submitCity")
+    if (state.city === "Seattle") {
+        const userCity = document.querySelector("#userCity").value;
+        state.city=`${userCity}`;
+        state.cityButton = "Reset"
+    } else {
+        state.cityButton = "Submit";
+        state.city = "Seattle"
+    }
+    cityButtonElement.textContent = `${state.cityButton}`
     const cityElement = document.querySelector("#defaultCity");
     cityElement.textContent = `How is the weather today in ${state.city}?`;
-    state.cityButton = "Reset"
-    const cityButtonElement = document.querySelector("#submitCity")
-    cityButtonElement.textContent = `${state.cityButton}`
 }
-
-
 
 const registerEventHandlers = () => {
     const upTemperatureButton = document.querySelector("#upTemperature");
     upTemperatureButton.addEventListener("click", updateTemperature)
     const downTemperatureButton = document.querySelector("#downTemperature");
-    downTemperatureButton.addEventListener('click', downTemperature);
+    downTemperatureButton.addEventListener('click', downTemperature)
     const submitCityButton = document.querySelector("#submitCity");
     submitCityButton.addEventListener('click', updateCity)
-
+    
 }
 
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
