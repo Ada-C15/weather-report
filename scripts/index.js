16e3a25e24c3aefe0487f0b0018fc6f3fd1b2c99
Marisa <marisa.n.morales@gmail.com>
2021-06-15 21:34:46 -0700

    Adds JavaScript skeletons for Location Name and Location Reset. Refactors the Temp buttons.

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,66 @@
-const state = {
-    currentTemp: 65,
-    // defaultSky: sunny,
-    // defaultLocation: "Unceded Duwamish Lands"
-};
+let currentTemp = 65;
+    
 
 const skyImages = {
-    sunny: ["https://media.giphy.com/media/lyVNcb1n5Ob0Q/giphy.gif", "sunny alt"],
-    cloudy: ["https://media.giphy.com/media/lOkbL3MJnEtHi/giphy.gif", "cloudy alt"],
-    rainy: ["https://media.giphy.com/media/vLi3T5m3RH45y/giphy.gif", "rainy alt"],
-    snowy: ["https://media.giphy.com/media/Fh3ezinVpi4yk/giphy.gif", "snowy, alt"]
+    sunny: ["https://media.giphy.com/media/lyVNcb1n5Ob0Q/giphy.gif", "The sun shines directly into the camera lens through tall evergreens. The camera angle moves slightly."],
+    cloudy: ["https://media.giphy.com/media/lOkbL3MJnEtHi/giphy.gif", "The sun shines through clouds racing across the sky."],
+    rainy: ["https://media.giphy.com/media/vLi3T5m3RH45y/giphy.gif", "A hyper realistic animation. of heavy rain bouncing off tiled patio"],
+    snowy: ["https://media.giphy.com/media/Fh3ezinVpi4yk/giphy.gif", "A hyper realiztic animation of heavy snow falling through evergreens against distant mountains."]
 };
 
-const addTemp = (event) => {
-    state.currentTemp += 1;
+// CHANGE TO FIT
+const updateTemp = currentTemp => {
     const temperatureCountContainer = document.querySelector("#temperatureCount")
-    temperatureCountContainer.textContent = `Temperature ${state.currentTemp}℉`;
+    temperatureCountContainer.textContent = `Temperature ${currentTemp}℉`;
+    changeTempColor(currentTemp);
+    // updateGarden(currentTemp);
+}
+// Figuring out text / temp changes
+const changeTempColor = (currentTemp) => {
+    const temperatureCountContainer = document.getElementById("temperatureCount");
+    let color = 'blue'
+    if (currentTemp >= 80) {
+        color = 'red';
+    } else if (currentTemp >= 70) {
+        color= 'orange';
+    } else if (currentTemp >= 60) {
+        color = 'yellow';
+    } else if (currentTemp >= 50) {
+        color = 'green';
+    }
+    temperatureCountContainer.classList = color;
+}; 
+
+const addTemp = (event) => {
+    currentTemp += 1;
+    updateTemp(currentTemp);
 };
 const subtractTemp = (event) => {
-    state.currentTemp -= 1;
-    const temperatureCountContainer = document.querySelector("#temperatureCount")
-    temperatureCountContainer.textContent = `Temperature ${state.currentTemp}℉`;
+    currentTemp -= 1;
+    updateTemp(currentTemp);
 };
-const changeLandscape = (event) => {
-    // skyImageContainer.removeChild(x);
+const changeSky = (event) => {
     let option = document.querySelector('#sky');
     let optionValue = option.value;
-    var x = document.createElement("IMG");
-    x.setAttribute("src", skyImages[optionValue][0]);
-    x.setAttribute("alt", skyImages[optionValue][1]);
-    skyImageContainer.replaceChild(x, skyImageContainer.childNodes[0]);
-    // skyImageContainer.appendChild(x);
+    var skyImage = document.createElement("IMG");
+    skyImage.setAttribute("src", skyImages[optionValue][0]);
+    skyImage.setAttribute("alt", skyImages[optionValue][1]);
+    skyImageContainer.replaceChild(skyImage, skyImageContainer.childNodes[0]);
 };
+// NEED HTML
+// const updateCityName = () => {
+//     const inputName = document.getElementById("cityNameInput").value;
+//     const headerCityName = document.getElementById("headerCityName");
+//     headerCityName.textContent = inputName;
+// };
+
+// NEED HTML
+// const resetCityName = () => {
+//     const cityNameInput = document.getElementById("cityNameInput");
+//     cityNameInput.value = "Seattle";
+//     updateCityName();
+// };
+
 
 const registerEventHandlers = (event) => {
     const addTemperatureButton = document.querySelector("#addTemperatureButton");
@@ -39,8 +69,23 @@ const registerEventHandlers = (event) => {
     const subtractTemperatureButton = document.querySelector("#subtractTemperatureButton");
     subtractTemperatureButton.addEventListener("click", subtractTemp);
 
-    const landscapeOptions = document.querySelector("#sky");
-    landscapeOptions.addEventListener("change", changeLandscape);
+    const skyOptions = document.querySelector("#sky");
+    skyOptions.addEventListener("change", changeSky);
+
+    // CHANGE TO FIT
+    updateCityName();
+    // const cityNameInput = document.getElementById("cityNameInput");
+    // cityNameInput.addEventListener("input", updateCityName);
+
+    // CHANGE TO FIT
+    // const cityNameResetBtn = document.getElementById("cityNameReset");
+    // cityNameResetBtn.addEventListener("click", resetCityName);
+
+
+    // Figuring out text / temp changes
+    // const changeTextColor = document.getElementsByClassName("tempButton");
+    // changeTextColor.addEventListener("change", changeTempColor);
+
 
 };
 
