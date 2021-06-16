6ec06cb6ed5fe5d09c4b5022681cf22e36ea5ad6
Raquel Mena <raquelmena03@gmail.com>
2021-06-15 21:53:18 -0500

    updated javascript/html

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,47 @@
 console.log('hello world')
 
+const state = {
+    tempCount: 70,
+};
 
-function increaseValue() {
-    var value = parseInt(document.getElementById('number').value, 10);
-    value = isNaN(value) ? 0 : value;
-    value++;
-    document.getElementById('number').value = value;
+const addTemp = () => {
+    const weather = document.querySelector("#weather-content");
+    // const weatherGarden = document.querySelector("#weather-garden");
+    if (state.tempCount < 80) {
+        weather.textContent = "🌞🌞🌞🌞";
+        // weatherGarden.innerText(weather);
+    } else if (90 < state.tempCount < 95) {
+        weather.textContent = "😭😭😭😭";
+        // weatherGarden.appendChild(weather);
+    }
+    state.tempCount += 1;
+    const tempContainer = document.querySelector("#tempCount")
+    tempContainer.textContent = `${state.tempCount}`;
 }
 
-function decreaseValue() {
-    var value = parseInt(document.getElementById('number').value, 10);
-    value = isNaN(value) ? 0 : value;
-    value < 1 ? value = 1 : '';
-    value--;
-    document.getElementById('number').value = value;
+const decreaseTemp = () => {
+    const weather = document.createElement("span");
+    const weatherGarden = document.querySelector("#weather-garden");
+    weather.textContent = "🙏🏽";
+    weatherGarden.appendChild(weather);
+    state.tempCount -= 1;
+    const tempContainer = document.querySelector("#tempCount")
+    tempContainer.textContent = `${state.tempCount}`;
 }
+const skySelectChange = (event) => {
+    const result = document.querySelector('#sky-select');
+    result.textContent = `🌞🌞😭😭😭😭`;
+}
+
+const registerEventHandlers = () => {
+    const addTempButton = document.querySelector("#addTempButton");
+    addTempButton.addEventListener("click", addTemp);
+    const decreaseTempButton = document.querySelector("#decreaseTempButton");
+    decreaseTempButton.addEventListener("click", decreaseTemp);
+    const skySelecter = document.querySelector('#sky-select');
+    skySelecter.addEventListener("change", skySelectChange);
+
+
+}
+
+document.addEventListener("DOMContentLoaded", registerEventHandlers);
