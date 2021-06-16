ed0305a567b34d0d1bd53d30fcec96aac4cb7168
Libby L. Gerber <libby@libbygerber.com>
2021-06-16 12:04:14 -0700

    added js

--- scripts/index.js
+++ scripts/index.js
@@ -17,6 +17,33 @@ const updateSky = () => {
     }
     skyContainer.textContent = sky;
     const gardenContent = document.getElementById("gardenContent")
-    
+    gardenContent.classList = `garden__content ${skyColor}`
+};
 
+const updateCityName = () => {
+    const inputName = document.getElementById("cityNameInput").value;
+    const headerCityName = document.getElmentById("headerCityName");
+    headerCityName.textContent = inputName;
+};
+
+const resetCityName = () => {
+    const cityNameInput = document.getElmentById("cityNameInput");
+    cityNameInput.value = "Seattle";
+    updateCityName();
+};
+
+const updateTempStyles = (currentTemp) => {
+    const tempValueContainer = document.getElementById("tempValue");
+    tempValueContainer.textContent = tempValue;
+    let color = "teal";
+    if ( currentTemp >= 80) {
+        color = "red";
+    } else if (currentTemp >=70) {
+        color = "orange";
+    } else if (currentTemp >= 60) {
+        color = "yellow";
+    } else if (currentTemp >= 50) {
+        color = "green";
+    }
+    tempValueContainer.classList = color;
 }
