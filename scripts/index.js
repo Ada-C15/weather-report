81591cbe8295d2c7f4af2882af13016e450b052e
Heidi Zhang <qingyuanhzhang@gmail.com>
2021-06-15 20:29:25 -0700

    wave 3 completed

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,8 @@
 const state = {
     temperature : 71,
     color : "orange",
-    landscape : "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷"
+    landscape : "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷",
+    sky : "☁️ ☁️ ☁️ ☀️ ☁️ ☁️"
 }; 
 
 const colorAppear = () => {
@@ -14,33 +15,31 @@ const landScape = () => {
     const landScape = document.querySelector("#landscape")
     landScape.textContent = state.landscape
 }
+
+const skyDisplay = () => { 
+    const skyDisplay = document.querySelector("#sky")
+    skyDisplay.textContent = state.sky
+}
+
 const tempRangeColor = () => {
     if (state.temperature >= 80){
         state.color = "red"
         state.landscape = "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂"
-        colorAppear();
-        landScape();
     } else if (state.temperature < 80 && state.temperature >= 70) {
         state.color = "orange"
         state.landscape = "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷"
-        colorAppear();
-        landScape();
     } else if (state.temperature < 70 && state.temperature >= 60) {
         state.color = "yellow"
         state.landscape = "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃"
-        colorAppear();
-        landScape();
     } else if (state.temperature < 60 && state.temperature >= 50) {
         state.color = "green"
         state.landscape = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲"
-        colorAppear();
-        landScape();
     } else if (state.temperature < 50) {
         state.color = "teal"
         state.landscape = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲"
-        colorAppear();
-        landScape();
-    }
+    } 
+    colorAppear();
+    landScape();
 }
 
 const tempIncrease = () => {
@@ -61,12 +60,29 @@ const tempDecrease = () => {
     tempRangeColor()
 };
 
+const skySelect = () => {
+    const selectSky = document.querySelector("#skySelect")
+
+    if (selectSky.options[selectSky.selectedIndex].text === "Sunny") {
+        state.sky = "☁️ ☁️ ☁️ ☀️ ☁️ ☁️"
+    } else if (selectSky.options[selectSky.selectedIndex].text === "Cloudy") {
+        state.sky = "☁️☁️ ☁️ ☁️☁️ ☁️ 🌤 ☁️ ☁️☁️"
+    } else if (selectSky.options[selectSky.selectedIndex].text === "Rainy") {
+        state.sky = "🌧🌈⛈🌧🌧💧⛈🌧🌦🌧💧🌧🌧"
+    } else if (selectSky.options[selectSky.selectedIndex].text === "Snowy") {
+        state.sky = "🌨❄️🌨🌨❄️❄️🌨❄️🌨❄️❄️🌨🌨"
+    }
+    skyDisplay()
+}
 const registerTempHandler = () => {
     const increaseButton = document.querySelector("#increaseTempControl");
     increaseButton.addEventListener("click", tempIncrease);
 
     const decreaseButton = document.querySelector("#decreaseTempControl");
     decreaseButton.addEventListener("click", tempDecrease);
+    
+    const selectSky = document.querySelector("#skySelect");
+    selectSky.addEventListener("change", skySelect);
 }
 
-document.addEventListener("DOMContentLoaded", registerTempHandler)
+document.addEventListener("DOMContentLoaded", registerTempHandler);
