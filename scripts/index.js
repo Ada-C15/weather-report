22fc0340bb3d7677be637bb3d393500c8b9e41a6
Juliana DeCarvalho Sutherland <juliana.decarvalho@wsu.edu>
2021-06-16 12:54:38 -0700

    temp section created

--- scripts/index.js
+++ scripts/index.js
@@ -12,11 +12,7 @@ const registerEventHandlers = () => {
     mountainNameInput.addEventListener('input', updateMountain)
     const resetButton = document.querySelector("#resetButton");
     resetButton.addEventListener('click', resetMountain)
-
-
-
-
-}
+};
 
 document.addEventListener("DOMContentLoaded", registerEventHandlers)
 
@@ -29,4 +25,30 @@ const state = {
 };
 
 
-// const  = () => {}
+const  tempValue= () => {
+    if (state.currentTemp >= 80){
+        return ["#F11514", `"🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂"`];
+    }
+    else if (state.currentTemp >= 70 && state.temperature <= 79){
+        return ["#FF7F50", `"🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷"`]
+    }
+    else if (state.currentTemp >= 60 && state.currentTemp <= 69){
+        return ["#DAA520", `"🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃"`]
+    }
+    else if (state.currentTemp >=50 && state.currentTemp <= 59){
+        return ["#228B22", `"🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲"`]
+    }
+    else (state.currentTemp < 49){
+        return ["#00BFFF", "⛷❄️❄️⛷❄️❄️⛷❄️❄️⛷❄️❄️⛷❄️❄️⛷❄️❄️⛷❄️"]
+    }
+}
+
+const increaseTemperature = () => {
+    state.currentTemp +=1;
+    const tempValueElement = document.querySelector("#tempValue");
+    const gardenLandscape = document.querySelector("#landscape");
+    let tempColor = tempValue();
+    tempValueElement.style.color = tempColor[0]
+    tempValueElement.textContent = `${state.currentTemp}`;
+    gardenLandscape.textContent = tempColor[1];
+};
