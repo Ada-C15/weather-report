75361a56aad1edfb84c2078e3f351a9688625481
SterlingSunshine <katrina.kimzey@gmail.com>
2021-06-16 05:42:34 -0700

    Wave 3

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,14 @@
 const state = {
   temp: 73,
+  cloudy: "☁️☁️☁️_☁️☁️☁️☁️☁️☁️",
+  partlyCloudy: "☁️☁️___☀️☁️_☁️☁️",
+  rainy: "🌧🌧🌧🌧🌧🌧🌧🌧🌧🌧",
+  sunny: "___☀️☁️_____",
+  onFireSky: "☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️",
 }
 
 const changeTemp = (delta) => {
   state.temp += delta;
-  console.log(state.temp);
   const tempDisplay = document.querySelector("#tempDisplay");
   tempDisplay.textContent = `${state.temp}`;
 }
@@ -17,12 +21,22 @@ const decreaseTemp = () => {
   changeTemp(-1);
 }
 
+const skyDisplayUpdate = () => {
+  const updatedWeather = document.getElementById("cloudStatus").value;
+  const skyDisplay = document.getElementById("sky");
+  console.log(updatedWeather);
+  skyDisplay.textContent = state[updatedWeather];
+}
+
 const registerEventHandlers = () => {
   const increaseButton = document.querySelector("#tempIncreaseButton");
   increaseButton.addEventListener("click", increaseTemp);
   
   const decreaseButton = document.querySelector("#tempDecreaseButton");
   decreaseButton.addEventListener("click", decreaseTemp);
+
+  const changeWeather = document.getElementById("cloudStatus");
+  changeWeather.addEventListener("change", skyDisplayUpdate);
 }
 
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
