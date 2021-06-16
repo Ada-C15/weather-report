3294c34d83c4b2918b85a26e117af8f78475f38c
Brittney Payne <ohoptional@gmail.com>
2021-06-15 18:11:27 -0700

    added js logic to change background'

--- scripts/index.js
+++ scripts/index.js
@@ -21,11 +21,14 @@ let temp = 85;
 
 const increaseTempButton = document.querySelector('#tempInc');
 
+
 const increaseTemp = () => {
     const currentTempDisplay = document.querySelector('#tempDisplay');
     temp += 1;
     currentTempDisplay.textContent = temp;
-}
+    updateTempSection(temp)
+    }
+
 
 increaseTempButton.addEventListener("click", increaseTemp)
 
@@ -35,7 +38,64 @@ const decreaseTemp = () => {
     const currentTempDisplay = document.querySelector('#tempDisplay');
     temp -= 1;
     currentTempDisplay.textContent = temp;
+    updateTempSection(temp)
 }
 
 decreaseTempButton.addEventListener("click", decreaseTemp)
 
+/// temp color changing logic 
+
+const tempSection = document.querySelector('#tempSection');
+
+const updateTempSection = (temp) => {
+    if (temp > 85) {
+        tempSection.className = 'hot';
+    } else if (temp > 70 && temp <= 85) {
+        tempSection.className = 'warm';
+    } else if (temp > 60 && temp <= 70) {
+        tempSection.className = 'perfect';
+    } else if (temp > 50 && temp <= 60) {
+        tempSection.className = 'cool';
+    } else {
+        tempSection.className = 'cold';
+    }
+    } 
+
+
+///sky changing logic 
+
+
+
+const selectOptions = document.querySelector('#skySelect');
+
+const updateSkySection = () => {
+    const skySection = document.querySelector('#skySection');
+    let options = document.querySelector('#skySelect').value;
+    if (options === "sunny") {
+        skySection.className = 'sunny';
+        
+    } else if  (options === "cloudy") {
+        skySection.className = 'cloudy';
+    } else if (options === "rainy") {
+        skySection.className = 'rainy';
+    } else if (options === 'snowy') {
+        skySection.className = 'snowy';
+    } else {
+        skySection.className = 'stormy';
+    }
+    updateLandscape(skySection.className)
+
+
+}
+
+
+selectOptions.addEventListener('change', updateSkySection)
+
+
+//// landscape changing logic 
+
+const updateLandscape = (skySection) => {
+    const landscapeSection = document.querySelector('#landscape');
+    landscapeSection.className = skySection;
+
+}
