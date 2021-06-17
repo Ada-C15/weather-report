1cb0347b864fc8962253485c36926c7f7d2c5de4
alison <alimcginley@gmail.com>
2021-06-17 11:31:12 -0700

    updates reset functionality - readme comprehension error

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,5 @@
 const state = {
     temperature: 45,
-    city: "Seattle",
-    cityButton: "Submit",
     tempClasses: "box coldWeather",
     landscape: "",
     sky: "",
@@ -17,7 +15,7 @@ const changeTempBackground = () => {
         state.landscape = `🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂`;
     } else if (state.temperature >= 70 && state.temperature <= 79) {
         state.tempClasses = `box notTooHot`;
-        state.landscape = `🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷`;
+        state.landscape = `🌸🌿🌼__🌷🌻``🌿_☘️🌱_🌻🌷`;
     } else if (state.temperature >= 60 && state.temperature <= 69){
         state.tempClasses = `box quiteCool`;
         state.landscape = `🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃`;
@@ -52,8 +50,6 @@ const updateSky = () => {
     }
     skyDisplay.textContent=`${state.sky}`;
     background.className = state.backgroundClass;
-
-
 }
 
 //updates temperature state based on user input
@@ -61,7 +57,6 @@ const updateTemperature = () => {
     changeTempBackground();
     const temperatureElement = document.querySelector("#currentTemp");
     temperatureElement.textContent = `${state.temperature}`;
-
 }
 
 const upTemperature = () => {
@@ -76,18 +71,17 @@ const downTemperature = () => {
 
 //updates City header dependent on user input
 const updateCity = () => {
-    const cityButtonElement = document.querySelector("#submitCity")
-    if (state.city === "Seattle") {
-        const userCity = document.querySelector("#userCity").value;
-        state.city=`${userCity}`;
-        state.cityButton = "Reset"
-    } else {
-        state.cityButton = "Submit";
-        state.city = "Seattle"
-    }
-    cityButtonElement.textContent = `${state.cityButton}`
-    const cityElement = document.querySelector("#defaultCity");
-    cityElement.textContent = `How is the weather today in ${state.city}?`;
+    const input = document.querySelector("#userCity");
+    const city = document.querySelector("#defaultCity");
+    city.textContent = `How's the weather today in ${input.value}?`
+}
+
+//resets City to Seattle
+const resetCity = () => {
+    const city = document.querySelector("#defaultCity");
+    const input = document.querySelector("#userCity");
+    city.textContent = `How's the weather today in Seattle?`
+    input.value = ""
 }
 
 const registerEventHandlers = () => {
@@ -95,10 +89,12 @@ const registerEventHandlers = () => {
     upTemperatureButton.addEventListener("click", upTemperature)
     const downTemperatureButton = document.querySelector("#downTemperature");
     downTemperatureButton.addEventListener('click', downTemperature)
-    const submitCityButton = document.querySelector("#submitCity");
-    submitCityButton.addEventListener('click', updateCity)
+    const resetCityButton = document.querySelector("#resetCity");
+    resetCityButton.addEventListener('click', resetCity)
     const skySelector = document.querySelector("#skySelection");
     skySelector.addEventListener(`change`, updateSky)
+    const cityInput = document.querySelector("#userCity")
+    cityInput.addEventListener(`input`, updateCity)
     
 }
 
