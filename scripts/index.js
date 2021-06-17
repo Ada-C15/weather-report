8d04ca74789af83690cff1ac8e0bf3bc97c0bfd2
Aida <aida.rysbek@gmail.com>
2021-06-16 23:59:46 -0700

    Functionality for city search, update and rename

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,11 @@ const api = {
     baseurl: "https://api.openweathermap.org/data/2.5/"
 }
 
+//set evenListener on search box keypress event
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+
 //Key is looking to be pressed at 13, based on ASCII it's CR aka Enter-Return key
 function setQuery(evt) {
     if (evt.keyCode == 13) {
@@ -20,6 +25,44 @@ function getResults (query) {
     }).then(displayResults);
 }
 
-//set evenListener on search box keypress event
-const searchbox = document.querySelector('.search-box');
-searchbox.addEventListener('keypress', setQuery);
+function displayResults (weather) {
+    console.log(weather);
+    const city = document.querySelector('.location .city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    const now = new Date();
+    const date = document.querySelector('.location .date');
+    date.innerText = dateBuilder(now);
+    //temp
+    const temp = document.querySelector('.current #temp');
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+
+    const weather_elem = document.querySelector('.current .weather');
+    weather_elem.innerText = weather.weather[0].main;
+
+    const hilow = document.querySelector('.hi-low');
+    hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
+}
+
+function dateBuilder (date_attr) {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    const day = days[date_attr.getDay()];
+    const date = date_attr.getDate();
+    const month = months[date_attr.getMonth()];
+    const year = date_attr.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+}
+
+const updateCityName = () => {
+    const inputName = document.getElementById("inputSearchBox").value;
+    const headerCityName = document.getElementById("headerCity");
+    headerCityName.textContent = inputName;
+};
+
+const resetCityName = () => {
+    const resetSearchBox = document.getElementById("inputSearchBox");
+    resetSearchBox.value = "Search for a city";
+}
