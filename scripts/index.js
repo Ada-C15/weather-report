5988753686f65f56d10a3af26b51c19e078e6cb1
marielaxcruz <marielacrtzcruz@gmail.com>
2021-06-30 12:48:07 -0700

    added working buttons and gardens

--- scripts/index.js
+++ scripts/index.js
@@ -1,121 +1,139 @@
 // setting the default variable to 70 
+// how would I keep the degree ?
 const state = {
-    tempCount: 70
+    tempCount: 70,
 };
 
+const increaseTemp = (event) => {
+    state.tempCount += 1
+    // const count = document.querySelector("#tempCount");
+    // count.textContent = `Temperature: ${state.tempCount}`;
+    const count = document.querySelector("#num");
+    count.textContent = state.tempCount;
 
-const updateTemp = tempValue => {
-    const tempCountContainer = document.getElementById("tempValue");
-    tempCountContainer.textContent = tempValue;
-}
-// Wave 2 - Increase Temp Behavior
-const addNum = () => {
-    state.tempCount += 1;
-    updateTemp(tempValue);
+    if (state.tempCount <= 49) {
+        freezingDay();
+    } else if ((50 <= state.tempCount) && (state.tempCount <= 59)) {
+        coldDay();
+    } else if ((60 <= state.tempCount) && (state.tempCount <= 69))  {
+        coolDay();
+    } else if ((70 <= state.tempCount) && (state.tempCount <= 79)) {
+        warmDay();
+    } else if (state.tempCount >= 80) {
+        hotDay();
+    }
 };
-// Wave 2- Decrease Temp Behavior
-const subNum = () => {
+
+const decreaseTemp = (event) => {
     state.tempCount -= 1;
-    updateTemp(tempValue);
+    const count = document.querySelector("#num");
+    count.textContent = state.tempCount;
 
-
-
-
-
-
-// Wave 2 - Change Color of Temp
-const tempRanges = () => {
-    if (state.tempCount <= 49){
-        document.getElementById("tempValue").style.color = "#008080";
-    } else if (state.tempCount >= 50 && state.tempCount <= 59){
-        document.getElementById("tempValue").style.color = "#00FF00";
-    } else if (state.tempCount >= 60 && state.tempCount <= 69){
-        document.getElementById("tempValue").style.color = "#FFF933";
-    } else if (state.tempCount >= 70 && state.tempCount <= 79){
-        document.getElementById("tempValue").style.color = "#FF5A33";
-    } else (state.tempCount >= 80);{
-        document.getElementById("tempValue").style.color = "#F8422D";
+    if (state.tempCount <= 49) {
+        freezingDay();
+    } else if ((50 <= state.tempCount) && (state.tempCount <= 59)) {
+        coldDay();
+    } else if ((60 <= state.tempCount) && (state.tempCount <= 69))  {
+        coolDay();
+    } else if ((70 <= state.tempCount) && (state.tempCount <= 79)) {
+        warmDay();
+    } else if (state.tempCount >= 80) {
+        hotDay();
     }
-
-}
-
-// Wave 2 - Temperature Ranges Change Landscape
-const tempLandscapeRanges = () => {
-    if (state.tempCount <= 59){
-        document.getElementById("landscape")= "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
-    } else if (state.tempCount >= 60 && state.tempCount <= 69){
-        document.getElementById("landscape") = "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃";
-    } else if (state.tempCount >= 70 && state.tempCount <= 79){
-        document.getElementById("landscape") = "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷";
-    } else (state.tempCount >= 80);{
-        document.getElementById("landscape") = "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂";
-    }
+};
 
 
-}
+// these colors will change with the temperature 
+//// Wave 2 - Change Color of Temp
+//const tempRanges = (tempCount) => {
+//    const tempColorContainer = document.querySelector("#tempValue");
+//    if (tempCount <= 49){
+//        document.getElementById("tempValue").style.color = "#008080";
+//    } else if (tempCount >= 50 && tempCount <= 59){
+//        document.getElementById("tempValue").style.color = "#00FF00";
+//    } else if (tempCount >= 60 && tempCount <= 69){
+//        document.getElementById("tempValue").style.color = "#FFF933";
+//    } else if (tempCount >= 70 && tempCount<= 79){
+//        document.getElementById("tempValue").style.color = "#FF5A33";
+//    } else (tempCount >= 80);{
+//        document.getElementById("tempValue").style.color = "#F8422D";
+//    }
 
-// Wave 3 - Selecting the Sky
+//}
 
-//selectElement.addEventListener('change', (event) => {
-const updateSky = () => {
-    const chosenSky = document.getElementById("skySelect").value;
-    const skyContainer = document.getElementById("sky");
-    if (chosenSky === "Sunny"){
-        skyContainer  =  "☁️ ☁️ ☁️ ☀️ ☁️ ☁️";
-    }else if (chosenSky === "Cloudy"){ 
-        skyContainer = "☁️☁️ ☁️ ☁️☁️ ☁️ 🌤 ☁️ ☁️☁️";
-    }else if (chosenSky === "Rainy"){
-        skyContainer = "🌧🌈⛈🌧🌧💧⛈🌧🌦🌧💧🌧🌧";
-    }else if (chosenSky === "Snowy"){
-        skyContainer = "🌨❄️🌨🌨❄️❄️🌨❄️🌨❄️❄️🌨🌨";
-}
-}
+// the background will change with the temperature
+const freezingDay = (event) => {
+    const gardenContainer = document.querySelector("#gardenContainer");
+    gardenContainer.textContent = "🧊🌨️🧊🧊🌨️🧊🧊🌨️🧊🧊🌨️🧊🧊🌨️🧊";
+};
 
+const coldDay = (event) => {
+    const gardenContainer = document.querySelector("#gardenContainer");
+    gardenContainer.textContent = "🌲⛄️🍂🌲⛄️🍂🌲⛄️🍂🌲⛄️🍂🌲⛄️🍂🌲⛄️🍂🌲⛄️🍂";
+};
 
+const coolDay = (event) => {
+    const gardenContainer = document.querySelector("#gardenContainer");
+    gardenContainer.textContent = "⛅🌸🌱⛅🌸🌱⛅🌸🌱⛅🌸🌱⛅🌸🌱🌱⛅🌸🌱🌱⛅🌸🌱🌱";
+};
 
+const warmDay = (event) => {
+    const gardenContainer = document.querySelector("#gardenContainer");
+    gardenContainer.textContent = "🌻🌷🌼🌻🌷🌼🌻🌷🌼🌻🌷🌼🌻🌷🌼🌻🌷🌼🌻🌷🌼";
+};
 
-// Wave 4 --  Naming the City
-const updateCityName = () => {
-    const cityName = document.getElementById("cityNameInput").value;
-    const nameContainer = document.getElementById("cityName")
-    nameContainer.textContent = cityName;
-}
+const hotDay = (event) => {
+    const gardenContainer = document.querySelector("#gardenContainer");
+    gardenContainer.textContent = "🌞🌴🌞🌴🌞🌴🌞🌴🌞🌴🌞🌴🌞🌴🌞🌴🌞🌴🌞🌴";
+};
 
+// this will control the sky that shows up as the user selects their sky
+const selectSky = (event) => {
+    const result = document.querySelector('#result');
 
-
-// Wave 5 - Resetting the City name 
-const resetCity = (event) => {
-    const locationStatus = document.getElementById("locationStatus");
-    locationStatus.value = 'Sunny Seattle';
+    if (event.target.value == "snowy") {
+        result.textContent = "🌨❄️☃️❄️☃️🌨❄️☃️❄️☃️🌨❄️☃️❄️☃️🌨❄️☃️❄️☃️🌨❄️☃️❄️☃️";
+    } else if (event.target.value == "rainy") {
+        result.textContent = "⛈☔️⛈☔️⛈☔️⛈☔️⛈☔️⛈☔️⛈☔️⛈☔️⛈☔️⛈☔️⛈☔️⛈☔️";
+    } else if (event.target.value == "cloudy") {
+        result.textContent = "☁️ 🌫☁️ 🌫☁️ 🌫☁️ 🌫☁️ 🌫☁️ 🌫☁️ 🌫☁️ 🌫";
+    } else if (event.target.value == "sunny") {
+        result.textContent = "☀️ 🌤 ☀️🌤☀️ 🌤 ☀️🌤☀️ 🌤 ☀️🌤☀️ 🌤 ☀️🌤☀️ 🌤 ☀️ ";
     };
-    
-    }
+};
 
+// display city name
+const updateCityName = (event) => {
+    const cityName = document.querySelector('#city');
+    cityName.textContent = event.target.value;
+};
 
+//// Wave 5 - Resetting the City name 
+const resetCity = (event) => {
+    const cityName = document.querySelector("#city");
+    cityName.textContent = "Seattle";
+};
 
-    // Wave 3 - Selecting the Sky
-    updateSky();
-    const skySelect = document.getElementById("skySelect");
-    skySelect.addEventListener("change", updateSky);
-
-
-
-    
 // one place to register all our event handlers for the whole page
-    const registerEventHandlers = () => {
-        // Wave 2 - Increase Temp
-        const adduparrow = document.querySelector("adduparrow");
-        adduparrow.addEventListener("click", addNum);
-        };
-        // Wave 2 - Decrease Temp 
-        const adddownarrow = document.getElementById("adddownarrow");
-        adddownarrow.addEventListener("click", subNum);;
-    // resting the city
-        const cityNameResetButton = document.getElementById("locationReset");
-        cityNameResetButton.addEventListener("click", resetCity);
+const registerEventHandlers= (event) => {
+
+    const increaseButton = document.querySelector("#increaseTempButton");
+    increaseButton.addEventListener("click", increaseTemp);
+
+    const decreaseButton = document.querySelector("#decreaseTempButton");
+    decreaseButton.addEventListener("click", decreaseTemp);
+
+    const resetButton = document.querySelector("#resetButton");
+    resetButton.addEventListener("click", resetCity);
+    
+    const textInput = document.querySelector("#name");
+    textInput.addEventListener("input", updateCityName);
+
+    const skySelection= document.querySelector("#skies");
+    skySelection.addEventListener("change", selectSky);
+
+};
 
 
-
-
-// registering all the event handlers and exporting them     
+// registering all the event handlers and exporting them  
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
