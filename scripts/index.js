c43ed81aebc0c4bd88c38a0e2d094a86f70ef763
Jessica <jessica.w.roberts@gmail.com>
2021-12-08 23:02:28 -0800

    Add temp control buttons

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,14 @@
-const temperature = document.getElementById("temperature");
+const increaseTempButton = document.getElementById("upButton");
+const decreaseTempButton = document.getElementById("downButton");
+
+const deltaTemp = function (delta) {
+  const temperature = document.getElementById("tempValue");
+  temperature.textContent = parseInt(temperature.textContent) + delta;
+};
+
+increaseTempButton.addEventListener("click", () => deltaTemp(1));
+decreaseTempButton.addEventListener("click", () => deltaTemp(-1));
+
 let currentTemp = 32;
 
 const upButton = document.createElement("button");
