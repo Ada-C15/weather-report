97b87f713e91888a9cf41d5d1470c1e28d378827
Rebeca <muniz.movement@gmail.com>
2021-12-09 14:09:10 -0800

    wave 2 increment button

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,28 @@
-const increaseTemp = () => {
-  const newTemp = document.createElement("span");
-  const tempContainer = document.querySelector("#tempContainer");
-  newTemp.textContent = "	🌡";
-  tempContainer.appendChild(newTemp);
+//Temperature values elements that will change when clicked
+const tempContainer = document.getElementById('temperature-value');
+const increaseButton = document.getElementById('increaseTemp');
+const decreaseButton = document.getElementById('decreaseTemp');
+
+const state = {
+  tempCount: 76,
+  skyColor: "#324DF7",
 };
+//increases and decrease tempereature with one click 
+const addTemperature = () => {
+  state.tempCount += 1;
+  tempContainer.textContent = `${state.tempCount}`;
+}
+const decreaseTemperature = () => {
+  state.tempCount -= 1;
+  tempContainer.textContent = `${state.tempCount}`;
+}
+
+const registerEventHandlers = (event) => {
+  // const increaseButton = document.querySelector("#increaseButton");
+  increaseButton.addEventListener("click", addTemperature);
+  decreaseButton.addEventListener("click", decreaseTemperature);
+};
+
+
+
+document.addEventListener("DOMContentLoaded", registerEventHandlers);
